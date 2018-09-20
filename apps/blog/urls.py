7b5b17602822b33c3c64060903766e0d17e8a5bb
Rayan Sóstenes <rayancocos@gmail.com>
2018-09-20 19:30:34 -0300

    Add API using Django Rest Framework

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,11 +1,19 @@
-from django.urls import path
+from django.urls import path, include
 from apps.blog import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('post', views.PostModelViewSet)
+
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.PostSearchView.as_view(), name='index'),
+    path('', views.PostSearchView.as_view(), name='search'),
+    path('api/', include(router.urls)),
     path('<slug>', views.PostDetailView.as_view(), name='detail'),
 ]
+
 
 menu = [
     {'url': 'blog:index', 'title': 'Blog'},
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,5 +1,10 @@
+from django.db.models import Q
 from django.views.generic import ListView, DetailView
+from rest_framework.viewsets import ModelViewSet
+
+
 from .models import Post
+from .serializers import PostSerializer
 
 class IndexView(ListView):
     model = Post
@@ -11,3 +16,18 @@ class PostDetailView(DetailView):
     template_name = 'blog/detalhe.html'
     slug_field = 'slug'
     slug_url_kwarg = 'slug'
+
+
+class PostSearchView(IndexView):
+    def get_queryset(self):
+        original_qs = super().get_queryset()
+        termo = self.request.GET.get('q', None)
+        if termo is None:
+            return original_qs
+        query = Q(title__icontains=termo) | Q(body__icontains=termo)
+        return original_qs.filter(query)
+
+
+class PostModelViewSet(ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all() 
